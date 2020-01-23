8f7f1998045b3b91d89504d5132346ed154dac18
Lucas Bazemore <lucasabazemore@gmail.com>
2020-01-22 23:41:12 -0600

    did the search crap

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,8 +2,11 @@
 def linear_search(arr, target):
   
   # TO-DO: add missing code
+  for k in range(0, len(arr)):
+    if(arr[k] == target):
+      return k
 
-   return -1   # not found
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -16,15 +19,36 @@ def binary_search(arr, target):
   high = len(arr)-1
 
   # TO-DO: add missing code
+  
+  while(low < high):
+    middle = (low + high) // 2
+    if(arr[middle] == target):
+      return middle
+    else:
+      if(arr[middle] > target):
+        high = middle
+      else:
+        low = middle
+
 
   return -1 # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
+  if(low <= high):
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+    middle = (low+high) // 2
+
+    if len(arr) == 0:
+      return -1 # array empty
+    # TO-DO: add missing if/else statements, recursive calls
+    if(arr[middle] == target):
+      return middle
+
+    if(arr[middle] > target):
+      return binary_search_recursive(arr, target, low, middle - 1)
+    else:
+      return binary_search_recursive(arr, target, middle + 1, high)
+  
+  return -1
